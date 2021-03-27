c7193c1e0e3462d8299ed62ae3f78c8f2dca9e2d
chris31roc <chris31roc@yahoo.com>
2021-03-27 16:15:25 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
